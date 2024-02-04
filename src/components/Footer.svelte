b98d9346c2eb8704b13be31a765338ff15071517
Matthew Wall <matthew.wallt@gmail.com>
2024-02-03 18:57:37 -0700

    fix icons

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,16 +1,18 @@
 <script>
-  import { Instagram } from 'lucide-svelte';
+  import { Instagram, StickyNote } from 'lucide-svelte';
 </script>
 <div class="grid grid-cols-2 justify-between w-full px-4 py-3 md:px-6 md:py-4">
   <a rel="noreferrer" class="flex items-end text-sm text-black font-bold" target="_blank" href="https://www.instagram.com/stiffsleeve/">
     <Instagram />
   </a>
   <div class="flex flex-col gap-2 justify-between items-end">
-    <a class="text-sm text-neutral-600 font-bold" href="/contact">
-      > Contact
+    <a class="text-sm text-neutral-600 flex gap-2 items-center font-bold" href="/contact">
+      <StickyNote class="w-4 h-4" />
+      Contact
     </a>
-    <a class="text-sm text-neutral-600 font-bold" href="/return-policy">
-      > Return Policy
+    <a class="text-sm text-neutral-600 flex gap-2 items-center font-bold" href="/return-policy">
+      <StickyNote class="w-4 h-4" />
+      Return Policy
     </a>
     <p class="text-sm text-neutral-600 font-bold">
       © {new Date().getFullYear()} Stiff Sleeve Co.

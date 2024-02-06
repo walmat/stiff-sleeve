7765be71a38cd99d2f24ae79d5624580838ac428
Matthew Wall <matthew.wallt@gmail.com>
2024-02-06 13:23:17 -0700

    remove padding except on small phones

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ const config = {
 			}
 		},
 		extend: {
+			screens: {
+				'tall': { 'raw': '(min-height: 674px)' },
+			},
 			colors: {
 				border: "hsl(var(--border))",
 				input: "hsl(var(--input))",
